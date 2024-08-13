939accefc4de5d28aa93a1ba66cc76cb2f882589
irvaguileradev7 <122749432+irvaguileradev7@users.noreply.github.com>
2024-08-13 15:46:27 -0600

    ,

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    host: true, 
-    port: 3000, 
-  }
 });
